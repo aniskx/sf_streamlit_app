045310f4def1b0783015f0c695663bf2325542be
Anis kheloufi <37309873+aniskx@users.noreply.github.com>
2023-02-03 14:23:01 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit 
+import pandas
 
 streamlit.title('Main menu')
 streamlit.header('Target')
@@ -7,3 +8,6 @@ streamlit.text('Data Modeling')
 streamlit.text('Cloud tech')
 
 streamlit.header('Platfroms')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
